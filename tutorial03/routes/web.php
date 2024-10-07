c4b530fd7bfd1c689e124f8c9d48bf01d1610931
DamienLovemore <matheusoaresmartins2020@gmail.com>
2024-10-07 10:13:35 -0300

    Tutorial 03 updates

--- tutorial03/routes/web.php
+++ tutorial03/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    //return view('welcome');
+    return view("home");
+})->name("home");
+
+Route::view("contact", "contact")->name("contact");
+Route::view("about", "about")->name("about");
+
+Route::view("/second", "second");
