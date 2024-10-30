23a3f45ebd0a2a2e52e27515e50735455ff01677
DamienLovemore <matheusoaresmartins2020@gmail.com>
2024-10-29 23:22:33 -0300

    Tutorial 03 Admin - Continuation

--- tutorial03-admin/database/seeders/DatabaseSeeder.php
+++ tutorial03-admin/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AdminSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test User 2',
+            'email' => 'test@testing.com',
         ]);
+
+        $this->call(AdminSeeder::class);
     }
 }
