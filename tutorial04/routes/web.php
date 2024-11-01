6646a6aead93227bfe9613b256bc122bdb584180
DamienLovemore <matheusoaresmartins2020@gmail.com>
2024-11-01 10:14:22 -0300

    Tutorial04 - Continuation

--- tutorial04/routes/web.php
+++ tutorial04/routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::get("/", [WelcomeController::class, "welcome"])->name("welcome");
+
+Route::get('/welcome2', function () {
+    return view('welcome2');
 });
 
 Route::get('/dashboard', function () {
