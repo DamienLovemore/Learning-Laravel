a2001f5ef4d108232b1b6c4d35ca5e335992a6be
DamienLovemore <matheusoaresmartins2020@gmail.com>
2025-08-29 14:00:13 -0300

    Tutorial 11 API - Continuation

--- tutorial11-api/database/seeders/DatabaseSeeder.php
+++ tutorial11-api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Ticket;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Ticket::factory(100)
+            ->recycle($users)//Recycle assigns randomly from the variable
+            ->create();
     }
 }
--- tutorial11-api/routes/api.php
+++ tutorial11-api/routes/api.php
@@ -2,9 +2,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 
 // php artisan install:api to enable API in the Laravel project (There should be a api.php file in routes)
 // url begins with api/
+// The Accept header defines the media types that the client is able to accept from the server. For instance, Accept: application/json, text/html indicates that the client prefers JSON or HTML responses. This information allows the server to send a resource representation that meets the client's needs.
 
 Route::get("/", function(){
     $data = ["message" => "Hello World!"];
@@ -15,6 +17,10 @@ Route::get("/", function(){
 Route::get("/loginmessage", [AuthController::class, "login_message"]);
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
+
+Route::get("/tickets", function(){
+    return Ticket::all();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
